7fdde79e13f51f061776801841cd2feb8147a645
black1ava <roeunboratharath@gmail.com>
2021-04-18 23:30:43 +0700

    Add server

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use('/assets', express.static('assets'));
 
 const PORT = process.env.POR || 3000;
 
-app.listen(3000, console.log('server is running on port ' + PORT));
+app.listen(PORT, console.log('server is running on port ' + PORT));
