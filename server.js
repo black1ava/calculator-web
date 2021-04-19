fbc8b26d5b1370f536ad583e604cd217a263f39a
black1ava <roeunboratharath@gmail.com>
2021-04-19 09:17:25 +0700

    Port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 app.use('/', express.static('client'));
 app.use('/assets', express.static('assets'));
 
-const PORT = process.env.POR || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log('server is running on port ' + PORT));
